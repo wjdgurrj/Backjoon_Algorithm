72869e9acbc80e3f83be185d5ee0e6814a29c749
Jeonghyeok Lee <59694789+wjdgurrj@users.noreply.github.com>
2020-10-08 14:48:55 +0900

    Create BOJ_2631.cpp

---
+++ Category/DP/BOJ_2631.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int v[201];
+int arr[201];
+
+int main(void) {
+	int n,result = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		v[i] = num;
+}
+	for (int i = 1; i <= n; i++) {
+		int cnt = 0;
+		for (int j = 0; j < i; j++) {
+			if (v[i] > v[j]) {
+				cnt = max(cnt, arr[j]);
+			}
+		}
+		arr[i] = cnt + 1;
+		result = max(result, arr[i]);
+	}
+	cout << n - result << endl;
+	return 0;
+}
