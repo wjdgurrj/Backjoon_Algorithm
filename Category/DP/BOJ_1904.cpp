b4bd75834b91dd7ddfdc7195115f40d88e2e16dd
Jeonghyeok Lee <59694789+wjdgurrj@users.noreply.github.com>
2020-10-08 14:43:27 +0900

    Create BOJ_1904.cpp

---
+++ Category/DP/BOJ_1904.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+long long d[1000001];
+int N;
+long long dp(int n) {
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 3;
+	for (int i = 4; i <= N; i++) {
+		d[i] =( d[i - 1] + d[i - 2]) % 15746;
+	}
+	return d[n];
+}
+int main(void) {
+	cin >> N;
+	cout << dp(N);
+	return 0;
+}
