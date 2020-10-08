64501594c5717613afc3efc3f7d87d26eb72ae7e
Jeonghyeok Lee <59694789+wjdgurrj@users.noreply.github.com>
2020-10-08 14:59:31 +0900

    Create BOJ__14852.cpp

---
+++ Category/DP/BOJ__14852.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+long long int d[1000001][2];
+
+long long int tyle(int x) {
+	d[0][0] = 0;
+	d[1][0] = 2;
+	d[2][0] = 7;
+	d[2][1] = 1;
+
+	for (int i = 3; i <= x; i++) {
+		d[i][1] = ( d[i - 1][1] + d[i-3][0]) % 1000000007;
+		d[i][0] = (3 * d[i - 2][0] + 2 * d[i - 1][0] +2* d[i][1]) % 1000000007;
+	}
+	int result = d[x][0];
+	return result;
+}
+
+int main(void) {
+	int x;
+	cin >> x;
+	cout << tyle(x) << endl;
+
+	return 0;
+}
