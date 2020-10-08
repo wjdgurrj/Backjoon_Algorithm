66b28a2addeecccdd42805b2d1428f3e8aaaf9cb
Jeonghyeok Lee <59694789+wjdgurrj@users.noreply.github.com>
2020-10-08 14:34:51 +0900

    Create BOJ_1003.cpp

---
+++ Category/DP/BOJ_1003.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int f[20];
+int main(void) {
+	int x;
+	cin >> x;
+	f[0] = 1;
+	f[1] = 0;
+	for (int i = 2; i <= 41; i++) {
+		f[i] = f[i - 1] + f[i - 2];
+	}
+	int n;
+	for (int i = 0; i < x; i++) {
+		cin >> n;
+		cout << f[n] << ' ' << f[n + 1] << endl;
+	}
+	
+}
